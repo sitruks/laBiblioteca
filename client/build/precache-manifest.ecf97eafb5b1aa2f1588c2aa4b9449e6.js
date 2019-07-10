1920580a1a627822bf72820aecae14ed3e184f09
SITRUK <kurtis.santillanes@gmail.com>
2019-07-10 01:02:20 -0700

    adding solved in-class app as starter

---
+++ client/build/precache-manifest.ecf97eafb5b1aa2f1588c2aa4b9449e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d0027039e9cacc1cec3",
+    "url": "/static/css/main.0864ef7e.chunk.css"
+  },
+  {
+    "revision": "6d0027039e9cacc1cec3",
+    "url": "/static/js/main.7ab8ca73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08e60256e97de3e5b743",
+    "url": "/static/js/2.8006c6cb.chunk.js"
+  },
+  {
+    "revision": "d0cc9756029e7fb9f67d22eda926c062",
+    "url": "/index.html"
+  }
+];
